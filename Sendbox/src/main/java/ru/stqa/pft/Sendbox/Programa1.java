65bea18a13c819f442233cabdcbdc81d03d7f43e
KobuleiAndrii <KobuleiAndrii@ukr.net>
2018-12-22 21:39:29 +0200

    изминения

--- Sendbox/src/main/java/ru/stqa/pft/Sendbox/Programa1.java
+++ Sendbox/src/main/java/ru/stqa/pft/Sendbox/Programa1.java
@@ -4,5 +4,9 @@ public class Programa1 {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        int l=6;
+        int s = l*l;
+
+        System.out.println("Площадь квадрата с стороной " +l+"  равна " + s );
     }
 }
