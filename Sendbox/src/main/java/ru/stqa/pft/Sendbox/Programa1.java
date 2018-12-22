e47c59c488efc2de24b915a616a5fbaa774484c2
KobuleiAndrii <KobuleiAndrii@ukr.net>
2018-12-22 17:40:38 +0200

    удалить

--- Sendbox/src/main/java/ru/stqa/pft/Sendbox/Programa1.java
+++ Sendbox/src/main/java/ru/stqa/pft/Sendbox/Programa1.java
@@ -1,8 +1,8 @@
 package ru.stqa.pft.Sendbox;
 
 public class Programa1 {
-public static void main(String[] args){
+    public static void main(String[] args) {
 
-    System.out.println("Hello");
-}
+        System.out.println("Hello");
+    }
 }
